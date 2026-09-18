80b73e8e72c54f24073ead23ea3a36991b0d3336
comicdanquinn <comicdanquinn@gmail.com>
2026-09-18 10:12:32 -0700

    Refresh cache for v6.3.18 course update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sg-vs-par-v6-3-17";
+const CACHE_NAME = "sg-vs-par-v6-3-18";
 const ASSETS = ["./", "./index.html", "./index(1).html", "./metric_patch.js", "./quick_guide_patch.js", "./history_cleanup_patch.js", "./post_round_summary_patch.js", "./fairway_stats_patch.js", "./round_state_guard_patch.js", "./single_hole_mobile_patch.js", "./swaneset_courses_patch.js", "./bc_course_finder_patch.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
